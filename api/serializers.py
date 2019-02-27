e063b2a72f66121a22fe7b61448f590dcb2eb1fe
Seongtaek Lim <eh71@dhcp-nyc-464.eduroam.cornell.edu>
2019-02-27 16:35:07 -0500

    add auto backup for api models

--- api/serializers.py
+++ api/serializers.py
@@ -15,15 +15,15 @@ class SubmissionSerializer(serializers.BaseSerializer):
     return {
       'categoryId': obj.category.categoryId,
       'consented': obj.consented,
-      'description': obj.description,
-      'mediaHash': obj.mediaHash,
-      'mediaType': obj.mediaType,
-      'name': obj.name,
-      'note': obj.note,
-      'occupations': obj.occupations,
-      'photo': base64.b64encode(obj.photo).decode('utf-8'),
+      'description': obj.description if obj.description else None,
+      'mediaHash': obj.mediaHash if obj.mediaHash else None,
+      'mediaType': obj.mediaType if obj.mediaType else None,
+      'name': obj.name if obj.name else None,
+      'note': obj.note if obj.note else None,
+      'occupations': obj.occupations if obj.occupations else None,
+      'photo': base64.b64encode(obj.photo).decode('utf-8') if obj.photo else None,
       'photoMimeType': 'image/jpeg',
-      'placeOfBirth': obj.placeOfBirth,
+      'placeOfBirth': obj.placeOfBirth if obj.placeOfBirth else None,
       'published': obj.published,
       'submissionDate': '{}-{:02d}-{:02d}'.format(
         obj.submissionDate.year,
@@ -32,9 +32,9 @@ class SubmissionSerializer(serializers.BaseSerializer):
       ),
       'submissionId': obj.submissionId,
       'url': None,
-      'yearOfBirth': obj.yearOfBirth,
-      'yearsInNeighborhoodFrom': obj.yearsInNeighborhoodFrom,
-      'yearsInNeighborhoodTo': obj.yearsInNeighborhoodTo
+      'yearOfBirth': obj.yearOfBirth if obj.yearOfBirth else None,
+      'yearsInNeighborhoodFrom': obj.yearsInNeighborhoodFrom if obj.yearsInNeighborhoodFrom else None,
+      'yearsInNeighborhoodTo': obj.yearsInNeighborhoodTo if obj.yearsInNeighborhoodTo else None
     }
 
 class TermsOfConsentsSerializer(serializers.BaseSerializer):
@@ -51,7 +51,6 @@ class AdminEmailSerializer(serializers.BaseSerializer):
 
 class EventSerializer(serializers.BaseSerializer):
   def to_representation(self, obj):
-    pass
     return {
       'eventId': obj.eventId,
       'title': obj.title,
@@ -59,19 +58,19 @@ class EventSerializer(serializers.BaseSerializer):
         obj.date.year,
         obj.date.month,
         obj.date.day
-      ),
+      ) if obj.date else None,
       'time': '{:02d}:{:02d}:{:02d}'.format(
         obj.time.hour,
         obj.time.minute,
         obj.time.second
-      ),
-      'location': obj.location,
-      'description': obj.description,
-      'link1': obj.link1,
-      'link2': obj.link2,
-      'videoURL': obj.videoURL,
-      'mediaHash': obj.mediaHash,
-      'image': base64.b64encode(obj.image).decode('utf-8'),
+      ) if obj.time else None,
+      'location': obj.location if obj.location else None,
+      'description': obj.description if obj.description else None,
+      'link1': obj.link1 if obj.link1 else None,
+      'link2': obj.link2 if obj.link2 else None,
+      'videoURL': obj.videoURL if obj.videoURL else None,
+      'mediaHash': obj.mediaHash if obj.mediaHash else None,
+      'image': base64.b64encode(obj.image).decode('utf-8') if obj.image else None,
       'imageMimeType': 'image/jpeg',
       'hidden': obj.hidden
     }
--- api/signals.py
+++ api/signals.py
@@ -1,47 +1,61 @@
 from datetime import datetime
-from .models import Submission, Category, AdminEmail, Event, TermsOfConsent
-from .serializers import SubmissionSerializer, CategorySerializer, AdminEmailSerializer, EventSerializer, TermsOfConsentsSerializer
+from .models import Submission, Category, Event, AdminEmail, TermsOfConsent
+from .serializers import SubmissionSerializer, CategorySerializer, EventSerializer, TermsOfConsentsSerializer
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 import json
 import base64
 
-def backupAll():
+def backupAll(key):
   rawData = {
     'termsOfConsents': TermsOfConsent.objects.all(),
     'submissions': Submission.objects.all(),
-    'categories': Category.objects.all()
+    'categories': Category.objects.all(),
+    'events': Event.objects.all()
   }
   serializers = {
     'termsOfConsents': TermsOfConsentsSerializer,
     'submissions': SubmissionSerializer,
-    'categories': CategorySerializer
+    'categories': CategorySerializer,
+    'events': EventSerializer
   }
-  suffix = ''
-  for key in rawData:
-    fileName = 'api/management/commands/{}{}{}'.format(key, suffix, '.json')
-    outputList = []
-    for d in rawData[key]:
-      serialized = serializers[key](d).data
-      outputList.append(serialized)
-    with open(fileName, 'w', encoding='utf-8') as f:
-      f.write(json.dumps(outputList, sort_keys=True, indent=2))
-
-  # adminEmails = AdminEmail.objects.all()
-  # events = Event.objects.all()
+  suffix = '' # For debugging purpose
+  fileName = 'api/management/commands/{}{}{}'.format(key, suffix, '.json')
+  outputList = []
+  for d in rawData[key]:
+    serialized = serializers[key](d).data
+    outputList.append(serialized)
+  with open(fileName, 'w', encoding='utf-8') as f:
+    f.write(json.dumps(outputList, sort_keys=True, indent=2))
 
 @receiver(post_save, sender=Submission)
+def submissionUpdated(sender, instance, created, **kwargs):
+  backupAll('submissions')
+
 @receiver(post_save, sender=Category)
-@receiver(post_save, sender=AdminEmail)
+def categoryUpdated(sender, instance, created, **kwargs):
+  backupAll('categories')
+
 @receiver(post_save, sender=Event)
+def eventUpdated(sender, instance, created, **kwargs):
+  backupAll('events')
+
 @receiver(post_save, sender=TermsOfConsent)
-def backupAllInstances(sender, instance, created, **kwargs):
-  backupAll()
+def tocUpdated(sender, instance, created, **kwargs):
+  backupAll('termsOfConsents')
 
 @receiver(post_delete, sender=Submission)
+def submissionRemoved(sender, instance, **kwargs):
+  backupAll('submissions')
+
 @receiver(post_delete, sender=Category)
-@receiver(post_delete, sender=AdminEmail)
+def categoryRemoved(sender, instance, **kwargs):
+  backupAll('categories')
+
 @receiver(post_delete, sender=Event)
+def eventRemoved(sender, instance, **kwargs):
+  backupAll('events')
+
 @receiver(post_delete, sender=TermsOfConsent)
-def backupAllInstances(sender, instance, **kwargs):
-  backupAll()
+def tocRemoved(sender, instance, **kwargs):
+  backupAll('termsOfConsents')
